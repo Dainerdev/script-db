03ed45273a4d15f1850ae4453a03e7f7d256bd81
Dainerdev <florianarroyo15@gmail.com>
2026-08-16 16:16:47 -0500

    Se implementa la funcion "read_excel_file" para la lectura de archivos excel

--- src/diagnostic.py
+++ src/diagnostic.py
@@ -14,32 +14,23 @@ ARCHIVE_DIAGNOSTIC = "archive/diagnostic_results.xlsx"
 
 
 # READ EXCEL FILES
-
-try:
-    # Read the Excel file into a DataFrame
-    df = pd.read_excel(ARCHIVE_EXCEL, engine="openpyxl")
-
-    # Display the first few rows of the DataFrame
-    print("DataFrame loaded successfully:\n")
-    print(df.head(n=5))  # Display the first 5 rows
+def read_excel_file(file_path):
+    """
+    Function to read an Excel file into a DataFrame
+    """
     
-except FileNotFoundError:
-    print(f"Error: File not found - {ARCHIVE_EXCEL}")
+    try:
+        # Read the Excel file into a DataFrame
+        df = pd.read_excel(file_path, engine="openpyxl")
+        return df
     
-except Exception as e:
-    print(f"Error reading Excel file: {e}")
-    exit()
-
-# GENERAL INFORMATION
-
-rows = len(df)
-columns = len(df.columns)
-
-print("\nINFORMACIÓN GENERAL\n") 
-
-print(f"Filas: {rows:,}") 
-print(f"Columnas: {columns:,}") 
-print(f"Tamaño: {rows:,} x {columns:,}")
+    except FileNotFoundError:
+        print(f"Error: File not found - {file_path}")
+        return None
+    
+    except Exception as e:
+        print(f"Error reading Excel file: {e}")
+        return None
 
 
 # FUNCTIONS
@@ -86,15 +77,5 @@ def standarize_column_dates(serie):
     dates = pd.to_datetime(serie, errors="coerce", dayfirst=True)
     
     return dates.dt.strftime("%d-%m-%Y")
-
-# IMPLMENTATION
-
-# Standardize column names
-# in the "Nombres" column of the DataFrame
-#df["Nombres"] = standarize_column_names(df["Nombres"])
-
-# Standardize column dates
-# in the "Fecha" column of the DataFrame
-df["Fecha"] = standarize_column_dates(df["Fecha"])
 
 
