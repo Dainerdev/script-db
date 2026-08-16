8b9c403be232511d4af3616b4ecbf28d421f8f82
Dainerdev <florianarroyo15@gmail.com>
2026-08-16 15:49:28 -0500

    Se desarrolla la funcion "standarize_column_names" en conjunto con "remove_accents" para la estandarización de nombres

--- src/diagnostic.py
+++ src/diagnostic.py
@@ -1,3 +1,4 @@
+import unicodedata
 import pandas as pd
 import numpy as np
 import re
@@ -39,5 +40,46 @@ print("\nINFORMACIÓN GENERAL\n")
 print(f"Filas: {rows:,}") 
 print(f"Columnas: {columns:,}") 
 print(f"Tamaño: {rows:,} x {columns:,}")
+
+
+# FUNCTIONS
+
+# Function to standarize column names
+def standarize_column_names(serie):
+    """
+    Function to standarize column names
+    """    
+    
+    # Ensure the series is of string type
+    serie = serie.astype(str)  
+    
+    # Remove leading and trailing whitespace
+    serie = serie.str.strip()
+    
+    # Replace spaces with underscores
+    serie = serie.str.replace(r"\s+", " ", regex=True)  # Replace multiple spaces with a single space
+    
+    # Convert to uppercase
+    serie = serie.str.upper()
+    
+    # Normalize unicode characters to ASCII
+    serie = serie.map(remove_accents)
+    
+    return serie
+
+# Remove accents from a string
+def remove_accents(s):
+    """
+    Function to remove accents from a string
+    """
+    
+    # Normalize the string to NFKD form and encode to ASCII, ignoring errors
+    return unicodedata.normalize("NFKD", s).encode("ASCII", "ignore").decode("ASCII")
+
+
+# IMPLMENTATION
+
+# Standardize column names
+df["Nombres"] = standarize_column_names(df["Nombres"])
 
 
