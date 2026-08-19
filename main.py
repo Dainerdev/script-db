4ccc4eda734626ade71ecf14f9b594e5aeb63df2
Dainerdev <florianarroyo15@gmail.com>
2026-08-19 12:56:36 -0500

    Organización

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.reading import *
-from src.cleaning import *
+from standarization import *
 from src.exportation import *
 from src.diagnostic import *
 
@@ -23,30 +23,40 @@ def main():
     # Get general information about the Excel file
     excel_general_information(df)
     
-    result_col = check_columns(df)
-    print("\nANÁLISIS DE COLUMNAS\n")
-    print(result_col.to_string(index=False))
     
-    result_nulls = check_nulls(df)
-    print("\nANÁLISIS DE VALORES NULOS\n")
-    print(result_nulls.to_string(index=False))
+    # CHECK IMPLEMENTATION
     
-    result_uniques = check_unique_values(df)
-    print("\nANÁLISIS DE VALORES ÚNICOS\n")
-    print(result_uniques.to_string(index=False))
+    # print("\nPROCESANDO ANÁLISIS DETALLADO\n")
+        
+    # result_col = check_columns(df)
+    # print("\nANÁLISIS DE COLUMNAS\n")
+    # print(result_col.to_string(index=False))
+    
+    # result_nulls = check_nulls(df)
+    # print("\nANÁLISIS DE VALORES NULOS\n")
+    # print(result_nulls.to_string(index=False))
+    
+    # result_uniques = check_unique_values(df)
+    # print("\nANÁLISIS DE VALORES ÚNICOS\n")
+    # print(result_uniques.to_string(index=False))
 
-    result_duplicates = check_duplicates(df)
-    print("\nANÁLISIS DE FILAS DUPLICADAS\n")
-    print(f"Registro total de filas duplicadas: {len(result_duplicates)}\n")
-    print(result_duplicates.to_string(index=False))
+    # result_duplicates = check_duplicates(df)
+    # print("\nANÁLISIS DE FILAS DUPLICADAS\n")
+    # print(f"Registro total de filas duplicadas: {len(result_duplicates)}\n")
+    # print(result_duplicates.to_string(index=False))
     
-    result_text_problems = check_text_problems(df)
-    print("\nANÁLISIS DE PROBLEMAS EN TEXTO\n")
-    print(result_text_problems.to_string(index=False))
+    # result_text_problems = check_text_problems(df)
+    # print("\nANÁLISIS DE PROBLEMAS EN TEXTO\n")
+    # print(result_text_problems.to_string(index=False))
     
-    result_length = check_lengths(df)
-    print("\nANÁLISIS DE LONGITUD DE TEXTO\n")
-    print(result_length.to_string(index=False))
+    # result_length = check_lengths(df)
+    # print("\nANÁLISIS DE LONGITUD DE TEXTO\n")
+    # print(result_length.to_string(index=False))
+    
+    
+    # STANDARIZATION IMPLEMENTATION
+    
+    #print("\nPROCESANDO ESTANDARIZACIÓN\n")
 
     # Standardize columns    
     # in the "Nombres" column of the DataFrame
@@ -54,7 +64,8 @@ def main():
     
     # in the "Fecha" column of the DataFrame
     #df["FECHA"] = standarize_column_dates(df["FECHA"])
-
+    
+    # SAVE EXCEL MODIFIED
     # Save the modified DataFrame to a new Excel file
     #export_excel(df, FILE_DIAGNOSTIC)
     
