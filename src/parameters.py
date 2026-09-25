3f73c40ca1921144740813f8b3cba3e1635038a2
Dainerdev <florianarroyo15@gmail.com>
2026-09-25 13:43:49 -0500

    Se establecen los parámetros que serán utilizados

---
+++ src/parameters.py
@@ -0,0 +1,41 @@
+# ============================================================
+# PARÁMETROS CONOCIDOS
+# ============================================================
+
+PARAMETROS_TIPO_EXPEDIENTE = [
+    "FÍSICO",
+    "ELECTRÓNICO",
+    "HÍBRIDO",
+    "POR DETERMINAR"
+]
+
+
+# ============================================================
+# COLUMNAS QUE REQUIEREN IDENTIFICACIÓN DE PARÁMETROS
+# ============================================================
+
+COLUMNAS_PARAMETROS = [
+    "TIPO DE EXPEDIENTE",
+    "NOMBRES_APELLIDOS",
+    "IDENTIFICACIÓN",
+    "SALA",
+    "SOLICITUD",
+    "DECISIÓN",
+    "PERTENECE",
+    "MAGISTRADO",
+    "DELEGADA",
+    "FUNCIONARIO A CARGO"
+]
+
+
+# ============================================================
+# COLUMNAS DE TEXTO LIBRE
+# ============================================================
+
+COLUMNAS_TEXTO_LIBRE = [
+    "DATOS RESOLUCION",
+    "OBSERVACIONES",
+    "RELACION MACROCASO O MEDIDA CAUTELAR",
+    "TERRITORIO O BLOQUE",
+    "MOTIVO DE ASIGNACION"
+]
